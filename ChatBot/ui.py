bb4ca030ecf40e835fec72c0b9b3a1a66cfda389
yangmingzhuo <310499278@qq.com>
2022-05-04 20:11:17 +0800

    update requirement

--- ChatBot/ui.py
+++ ChatBot/ui.py
@@ -399,8 +399,8 @@ class Chatter:
     def __init__(self, ex):
         self.ui = ex
         # nltk word package
-        # nltk.download('punkt')
-        # nltk.download('wordnet')
+        nltk.download('punkt')
+        nltk.download('wordnet')
         self.model = load_model(self.ui.root + '\\Chatter\\model\\chatter_model.h5')
         self.knowledge = json.loads(open(self.ui.root + '\\Chatter\\json\\knowledge.json').read())
         self.words = pickle.load(open(self.ui.root + '\\Chatter\\pkl\\words.pkl', 'rb'))
