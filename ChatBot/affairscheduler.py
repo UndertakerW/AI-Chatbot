212cbba46fbe017ef60108dd20135eb79667635e
UndertakerW <56337831+UndertakerW@users.noreply.github.com>
2022-05-04 15:25:22 +0800

    Add files for filter & scheduler

---
+++ ChatBot/affairscheduler.py
@@ -0,0 +1,105 @@
+import time
+
+
+def get_user_choice(text):
+    print('---------------------')
+    print('Please make a choice:')
+    print('1. Check my affair list')
+    print('2. Add an affair')
+    print('3. Modify an affair')
+    print('4. Delete an affair')
+    print('5. Exit')
+    user_input = int(text)
+    print('---------------------')
+    return user_input
+
+
+def warning_message():
+    print('WARNING: exiting the program will clear all your data!')
+    print('Continue? yes/no')
+    if input() == 'yes':
+        return True
+    return False
+
+
+def handle_user_request(choice, checklist, datelist):
+    if choice == 1:
+        if len(checklist) == 0:
+            print('Your list is empty!')
+        range_user_lists(checklist, datelist)
+        for i in range(0, len(checklist)):
+            print(i + 1, checklist[i], datelist[i], "is the ddl")
+        return checklist, datelist
+    if choice == 2:
+        entry_time = input('Type the ddl time here: ') #The input format must be month.date, like 5.20, 12.25
+        datelist.append(entry_time)
+        new_entry = input('Type you new to-do here: ')
+        checklist.append(new_entry)
+        print('Your affair has been added successfully!')
+        return checklist, datelist
+    if choice == 3:
+        if len(checklist) == 0:
+            print("Your list is empty! Nothing can be modified.")
+            return checklist, datelist
+        print('Which do you want to modify? Please enter its index.')
+        for i in range(0, len(checklist)):
+            print(i + 1, checklist[i], datelist[i])
+        index = int(input()) - 1
+        datelist[index] = input('Type your new time here: ')
+        checklist[index] = input('Type your new to-do here: ')
+        print('Your affair has been modified successfully!')
+        return checklist, datelist
+    if choice == 4:
+        if len(checklist) == 0:
+            print("Your list is empty! Nothing can be deleted.")
+            return checklist
+        print('Which do you want to delete? Please enter its index.')
+        for i in range(0, len(checklist)):
+            print(i + 1, checklist[i], datelist[i])
+        index = int(input()) - 1
+        del datelist[index]
+        del checklist[index]
+        print('Your affair has been deleted successfully!')
+        return checklist, datelist
+
+
+def range_user_lists(checklist, datelist):
+    localtime = time.localtime(time.time())
+    month = localtime[1]
+    day = localtime[2]
+    for i in range(0, len(datelist)):
+        ddl = datelist[i].split('.')
+        ddl_month = int(ddl[0])
+        ddl_day = int(ddl[1])
+        if month - ddl_month < 0:
+            checklist[i] = checklist[i] + " has Expired"
+        elif month - ddl_month == 0 and day - ddl_day > 0:
+            checklist[i] = checklist[i] + " has Expired"
+        elif month - ddl_month == 0 and day - ddl_day == 0:
+            checklist[i] = checklist[i] + " ★★★"
+            checklist[0], checklist[i] = checklist[i], checklist[0]
+            datelist[i] = 'Today'
+        elif month - ddl_month == 0 and day - ddl_day < 0:
+            checklist[i] = checklist[i] + " ★"
+        else:
+            pass
+
+
+def schedule(ui, text):
+    checklist = []
+    datelist = []
+    choice = get_user_choice(text)
+    if choice < 1 or choice > 5:
+        print('Invalid input!')
+    if choice == 5:
+        if warning_message():
+            print('Goodbye!')
+    else:
+        handle_user_request(choice, checklist, datelist)
+
+
+# Main function
+if __name__ == '__main__':
+    # schedule(2)
+    pass
+
---
+++ ChatBot/filter.py
@@ -0,0 +1,73 @@
+import nltk
+import joblib
+from nltk.tokenize import RegexpTokenizer, word_tokenize
+from nltk.stem import PorterStemmer
+from nltk.corpus import stopwords
+import pandas as pd
+from sklearn.feature_extraction.text import CountVectorizer
+
+from PyQt5.QtCore import QFileInfo
+root = QFileInfo(__file__).absolutePath()
+df = pd.read_csv(root+'\\spam.csv', encoding='ISO-8859-1')
+
+data = df.to_numpy()
+
+X = data[:, 1]  # X stores text emails
+tokenizer = RegexpTokenizer('\w+')
+nltk.download('stopwords')
+sw = set(stopwords.words('english'))  # ignore the stop words in english
+ps = PorterStemmer()
+
+
+def getStem(str):
+    str = str.lower()  # returns lowercased string from given string
+    # splits string into substrings of words
+    tokens = tokenizer.tokenize(str)
+    # stopwords are removed
+    removed_stopwords = [w for w in tokens if w not in sw]
+    # tokens  are reduced into root form
+    stemmed_words = [ps.stem(token) for token in removed_stopwords]
+    # join all stemmed words to single string
+    clean_review = ' '.join(stemmed_words)
+    return clean_review
+
+
+def getDoc(document):
+    d = []
+    for doc in document:
+        d.append(getStem(doc))
+    return d
+
+
+stemmed_doc = getDoc(X)
+
+cv = CountVectorizer()
+vc = cv.fit_transform(stemmed_doc)
+
+
+def update(emails):
+    up = getDoc(emails)
+    return cv.transform(up)
+
+
+model = joblib.load(root+"\\filter_model.m")
+
+
+def filterEmail(ui, email):
+    # 多段文本转换成一行字符串
+    email = ' '.join(email.split())
+
+    emails = []
+
+    emails.append(email)
+
+    emails = update(emails)
+
+    y_pred = model.predict(emails)
+
+    return y_pred[0]  # the return value type is <class 'numpy.str_'>
+
+
+if __name__ == '__main__':
+    pass
+    # filterEmail()
